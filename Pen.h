a59bbb7876a708b0ecae6006b22264f8747d752e
rmsgh1202 <54826629+rmsgh1202@users.noreply.github.com>
2019-10-02 16:17:59 +0900

    Add files via upload

---
+++ Pen.h
@@ -0,0 +1,33 @@
+﻿
+// Pen.h: Pen 애플리케이션의 기본 헤더 파일
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"       // 주 기호입니다.
+
+
+// CPenApp:
+// 이 클래스의 구현에 대해서는 Pen.cpp을(를) 참조하세요.
+//
+
+class CPenApp : public CWinApp
+{
+public:
+	CPenApp() noexcept;
+
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 구현입니다.
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CPenApp theApp;
---
+++ PenView.cpp
@@ -0,0 +1,160 @@
+﻿
+// PenView.cpp: CPenView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "Pen.h"
+#endif
+
+#include "PenDoc.h"
+#include "PenView.h"
+#include "CLine.h"
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CPenView
+
+IMPLEMENT_DYNCREATE(CPenView, CView)
+
+BEGIN_MESSAGE_MAP(CPenView, CView)
+	// 표준 인쇄 명령입니다.
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+	ON_WM_MOUSEMOVE()
+	ON_COMMAND(ID_MENU_COL, &CPenView::OnMenuCol)
+	ON_COMMAND(ID_SIZE_1, &CPenView::OnSize1)
+	ON_COMMAND(ID_SIZE_16, &CPenView::OnSize16)
+	ON_COMMAND(ID_SIZE_32, &CPenView::OnSize32)
+END_MESSAGE_MAP()
+
+// CPenView 생성/소멸
+
+CPenView::CPenView() noexcept
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+	size = 32;
+	col = RGB(0, 255, 0);
+}
+
+CPenView::~CPenView()
+{
+}
+
+BOOL CPenView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: CREATESTRUCT cs를 수정하여 여기에서
+	//  Window 클래스 또는 스타일을 수정합니다.
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CPenView 그리기
+
+void CPenView::OnDraw(CDC* pDC)
+{
+	CPenDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 여기에 원시 데이터에 대한 그리기 코드를 추가합니다.
+	int n = pDoc->m_oa.GetSize();
+	CLine* p;
+	for (int i = 0; i < n; i++) {
+		p = (CLine*)pDoc->m_oa[i];
+		p->Draw(pDC);
+	}
+}
+
+
+// CPenView 인쇄
+
+BOOL CPenView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 기본적인 준비
+	return DoPreparePrinting(pInfo);
+}
+
+void CPenView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄하기 전에 추가 초기화 작업을 추가합니다.
+}
+
+void CPenView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄 후 정리 작업을 추가합니다.
+}
+
+
+// CPenView 진단
+
+#ifdef _DEBUG
+void CPenView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CPenView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CPenDoc* CPenView::GetDocument() const // 디버그되지 않은 버전은 인라인으로 지정됩니다.
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CPenDoc)));
+	return (CPenDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CPenView 메시지 처리기
+
+//int n;
+void CPenView::OnMouseMove(UINT nFlags, CPoint point)
+{
+	if (nFlags == MK_LBUTTON) {
+		CLine* p = new CLine(pnt, point, size, col);
+		GetDocument()->m_oa.Add(p);
+		Invalidate(false);
+	}
+	pnt = point;
+	CView::OnMouseMove(nFlags, point);
+}
+
+
+void CPenView::OnMenuCol()
+{
+	// TODO: 여기에 명령 처리기 코드를 추가합니다.
+	CColorDialog dlg;
+	if (dlg.DoModal() == IDOK) {
+		col = dlg.GetColor();
+	}
+}
+
+
+void CPenView::OnSize1()
+{
+	size = 1;
+	// TODO: 여기에 명령 처리기 코드를 추가합니다.
+}
+
+
+void CPenView::OnSize16()
+{
+	// TODO: 여기에 명령 처리기 코드를 추가합니다.
+	size = 16;
+}
+
+
+void CPenView::OnSize32()
+{
+	// TODO: 여기에 명령 처리기 코드를 추가합니다.
+	size = 32;
+}
